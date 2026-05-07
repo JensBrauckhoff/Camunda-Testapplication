d8fb41a6dfc924ee5045f56fde9535a74add949d
Jens Brauckhoff <jens.brauckhoff@ilume.de>
2026-05-07 15:35:47 +0200

    Unittest für Prüf-DMN

---
+++ src/test/java/de/ilume/jens/testapplication/TestPruefDmn.java
@@ -0,0 +1,39 @@
+package de.ilume.jens.testapplication;
+
+import io.camunda.client.CamundaClient;
+import io.camunda.client.annotation.Deployment;
+import io.camunda.process.test.api.CamundaProcessTestContext;
+import io.camunda.process.test.api.CamundaSpringProcessTest;
+import io.camunda.process.test.api.TestDeployment;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
+
+@SpringBootTest
+@CamundaSpringProcessTest
+public class TestPruefDmn {
+
+    @Autowired
+    private CamundaClient client;
+    @Autowired
+    private CamundaProcessTestContext processTestContext;
+
+    @Test
+    @TestDeployment(resources = "Prüfauswertung.dmn")
+    public void testDmn() {
+        final Map<String, Object> variables = Map.of();
+        final var decisionResult1 = client.newEvaluateDecisionCommand()
+                .decisionId("Decision_Gesamtergebnis")
+                .variables(variables)
+                .send().join();
+        final var evaluations1 = decisionResult1.getEvaluatedDecisions();
+        System.out.println(evaluations1);
+    }
+
+    @SpringBootApplication
+    @Deployment(resources = "classpath*:/dmn/**/*.dmn")
+    static class TestProcessApplication {}
+}
